7ed3567c3f1837f97f80493754b0dedfa79d8875
demo <naganathdharwadkar20@gmail.com>
2026-08-02 11:52:14 +0530

    feat: build admin dashboard and doctor management

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import UserSync from "@/components/UserSync";
 import { Toaster } from "sonner";
+import TanStackProvider from "@/components/providers/TanStackProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <TanStackProvider>
     <ClerkProvider
       appearance={{
       variables: {
@@ -45,5 +47,6 @@ export default function RootLayout({
         </body>
       </html>
     </ClerkProvider>
+    </TanStackProvider>
   );
 }
